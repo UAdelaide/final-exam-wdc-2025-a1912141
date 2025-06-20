79549dba4fe3e41fc5ad3813beae952c0868dfcf
josheen <a1912141@adelaide.edu.au>
2025-06-20 01:56:53 +0000

    [automatic commit] 2025-06-20 01:56:53 UTC

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+const {
+    DB_HOST, DB_USER, DB_PASS, DB_NAME
+} = process.env;
 
 const pool = mysql.createPool({
   host: DB_HOST,
