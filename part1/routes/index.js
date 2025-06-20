863a12d3684d42e1f8456dcecf81a08cc1cb4ff7
josheen <a1912141@adelaide.edu.au>
2025-06-20 02:07:07 +0000

    [automatic commit] 2025-06-20 02:07:07 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -11,11 +11,11 @@ router.get('/api/dogs', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/dogs', function(req, res, next) {
+router.get('/api/walkrequests/open', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/dogs', function(req, res, next) {
+router.get('/api/walkers/summary', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
