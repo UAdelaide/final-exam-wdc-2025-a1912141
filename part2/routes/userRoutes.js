60b4482273c95739b2573d606a68d2a7497fb643
josheen <a1912141@adelaide.edu.au>
2025-06-20 10:55:27 +0000

    [automatic commit] 2025-06-20 10:55:27 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -59,12 +59,13 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// 
 router.post('/logout', (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       return res.status(500).json({ error: 'Logout failed' });
     }
-    res.clearCookie('connect.sid'); // default cookie name for express-session
+    res.clearCookie('connect.sid');
     res.json({ message: 'Logged out successfully' });
   });
 });
