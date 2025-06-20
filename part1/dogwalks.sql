e2836520bfe2ce5d0b20b38e1c2329ec463adc78
josheen <a1912141@adelaide.edu.au>
2025-06-20 01:30:55 +0000

    [automatic commit] 2025-06-20 01:30:55 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -60,4 +60,7 @@ VALUES
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('josheen01', 'josheen@example.com', 'hashed850', 'walker'),
-('bailey', 'bailey@example.com', 'hashed920', 'owner');
+('bailey01', 'bailey@example.com', 'hashed920', 'owner');
+
+INSERT INTO Dogs (owner_id, name, size)
+VALUES
