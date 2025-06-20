d2a7e147b5d423f541e31e4277325d39b0a3a8ff
josheen <a1912141@adelaide.edu.au>
2025-06-20 02:34:43 +0000

    [automatic commit] 2025-06-20 02:34:43 UTC

--- part1/app.js
+++ part1/app.js
@@ -20,6 +20,18 @@ const mysql = require('mysql2/promise');
       password: '',
       database: 'DogWalkService'
     });
+
+    await db.execute(`
+      CREATE TABLE IF NOT EXISTS Users (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        role ENUM('owner', 'walker') NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    `);
+
     } catch (err) {
     console.error('error in setting up: ', err);
   }
